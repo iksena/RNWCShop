99f048e79be660bfb58fca51a1d287f905324de2
I Komang Sena <i.buwana@btpn.com>
2020-05-30 11:07:58 +0700

    Add cart actions to containers

--- app/Containers/Detail/Detail.container.tsx
+++ app/Containers/Detail/Detail.container.tsx
@@ -1,12 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { useRoute, RouteProp } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { useDispatch } from 'react-redux';
+import { Action } from 'redux';
 
 import DetailComponent from './Detail.component';
 import { Product } from '../../Components/ProductItem/ProductItem.component';
 import WooCommerce from '../../Services/WooCommerce';
-import { NavigationParams } from '../../Navigations';
+import { NavigationParams, routes } from '../../Navigations';
+import { actions } from '../../Reducers/Cart.reducer';
 
-const DetailContainer = (): JSX.Element => {
+interface Props {
+  navigation: StackNavigationProp<NavigationParams>;
+}
+
+const DetailContainer = ({ navigation }: Props): JSX.Element => {
   const initialProduct = {
     id: 1,
     name: '',
@@ -18,9 +26,16 @@ const DetailContainer = (): JSX.Element => {
   const [product, setProduct] = useState<Product>(initialProduct);
   const [imagesShown, showImages] = useState(false);
   const route = useRoute<RouteProp<NavigationParams, 'Detail'>>();
+  const dispatch = useDispatch();
 
   const handlers = {
-    handleShowImages: (): void => showImages((prevState: boolean) => !prevState)
+    handleShowImages: (): void =>
+      showImages((prevState: boolean) => !prevState),
+    addToCart: (product: Product): Action => {
+      navigation.navigate('Orders', { screen: routes.Cart });
+
+      return dispatch(actions.addToCart(product));
+    }
   };
 
   useEffect(() => {
